41e08fbeb2e3151d4aef6f7ab0d8b9067cd52be7
Kristofer Liljeblad <kristol@microsoft.com>
2016-09-06 15:27:37 +0200

    Added basic projects to host analytics module in nether

---
+++ tests/Analytics/Nether.Analytics.Web.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nether.Analytics.Web.Tests
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
