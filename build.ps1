e5f13b65cff576093eccf48815a29341f09b69b6
Stuart Leeks <stuart.leeks@microsoft.com>
2016-09-06 08:07:10 +0200

    Add test execution to build scripts

--- build.ps1
+++ build.ps1
@@ -1,3 +1,13 @@
 dotnet restore
 
 Get-Content build\build-order.txt | ForEach-Object { dotnet build $_}
+
+$testExitCode = 0
+Get-Content build\test-order.txt | ForEach-Object { 
+    dotnet test $_
+    if ($LASTEXITCODE -ne 0){
+        $testExitCode = $LASTEXITCODE
+    }
+}
+
+exit $testExitCode
--- build.sh
+++ build.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
 # TODO - investigate the exception when running dotnet restore with parallelisation
+echo "*** Restoring packages"
 dotnet restore --disable-parallel
+
+
+echo
+echo "*** Building projects"
 
 while IFS= read -r var
 do
   dotnet build "$var"
 done < "build/build-order.txt"
+
+
+echo
+echo "*** Running tests"
+
+testExitCode=0
+while IFS= read -r var
+do
+  dotnet test "$var"
+  lastexit=$?
+  if [ $lastexit -ne 0 ]
+  then
+    testExitCode=$lastexit
+  fi
+done < "build/test-order.txt"
+
+exit $testExitCode
