91a5a5284aae81dd0084e15734b94ecb85705303
Stuart Leeks <stuart.leeks@microsoft.com>
2016-09-07 17:22:08 +0200

    Fix: build script doesn't stop on build error

--- build.ps1
+++ build.ps1
@@ -1,8 +1,29 @@
 dotnet --version
 
-dotnet restore
+$buildExitCode=0
 
-Get-Content build\build-order.txt | ForEach-Object { dotnet build $_}
+Write-Output "*** Restoring packages"
+dotnet restore
+$buildExitCode = $LASTEXITCODE
+if ($buildExitCode -ne 0){
+    Write-Output "*** Restore failed"
+    exit $buildExitCode
+}
+
+
+Write-Output "*** Building projects"
+$buildExitCode=0
+Get-Content build\build-order.txt | ForEach-Object { 
+   Write-Output "*** dotnet build $_"
+    dotnet build $_
+   if ($LASTEXITCODE -ne 0){
+       $testExitCode = $LASTEXITCODE
+   }
+}
+if($buildExitCode -ne 0) {
+    Write-Output "*** Build failed"
+    exit $buildExitCode
+}
 
 #$testExitCode = 0
 #Get-Content build\test-order.txt | ForEach-Object { 
