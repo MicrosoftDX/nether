0a97b743194df581b4e7e81106e89bb6f7c4071c
oliviak <liv.klose@outlook.com>
2016-09-07 11:34:31 +0200

    created project for basic api issue25

--- src/Analytics/Nether.Analytics.Web/Startup.cs
+++ src/Analytics/Nether.Analytics.Web/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Nether.Analytics.Data;
 
 namespace Nether.Analytics.Web
 {
