00767101e92131c9a01614d36e278287363e4120
Stuart Leeks <stuart.leeks@microsoft.com>
2016-09-08 16:35:25 +0200

    Update code structure
    Move Models to Models folder
    Remove unnecessary generics in Models
    Fix typo in ScoresListResponseModel (was ScoresListResponeModel)

---
+++ src/Leaderboard/Nether.Leaderboard.Web/Models/ScoresListResponeModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Nether.Leaderboard.Web.Models
+{
+    public class ScoresListResponseModel
+    {
+        public List<ScoreResponseModel> Leaderboard { get; set; }
+    }
+}
