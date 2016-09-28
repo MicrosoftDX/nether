e9ce8a8dbbc9de6002e55466ad7984b60c75bc1d
Kristofer Liljeblad <kristol@microsoft.com>
2016-09-28 11:46:13 +0200

    Fixed a the comments with regards to my PR

--- src/Nether.Integration.Default/Analytics/AnalyticsIntegrationNullClientConfigurationFactory.cs
+++ src/Nether.Integration.Default/Analytics/AnalyticsIntegrationNullClientConfigurationFactory.cs
@@ -4,6 +4,7 @@ using Nether.Integration.Analytics;
 
 namespace Nether.Integration.Default.Analytics
 {
+    //TODO: Fix and perhaps remove once DI work has progressed
     public class AnalyticsIntegrationNullClientConfigurationFactory : IDependencyFactory<IAnalyticsIntegrationClient>
     {
         public IAnalyticsIntegrationClient CreateInstance(IConfiguration configuration)
