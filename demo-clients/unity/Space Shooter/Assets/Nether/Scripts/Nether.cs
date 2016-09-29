db67cfca39d2efccf96f0c4320ff7e3b0e5c02d5
Kristofer Liljeblad <kristofer.liljeblad@live.com>
2016-09-29 14:19:34 +0200

    Added VSCode integration into demo

---
+++ demo-clients/unity/Space Shooter/Assets/Nether/Scripts/Nether.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nether : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("*** START ***");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
