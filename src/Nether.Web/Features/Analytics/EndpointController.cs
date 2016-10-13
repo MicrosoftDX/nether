f958ad7ca7bd392f400dfd5ecca2c93ae3753601
Stuart Leeks <stuart.leeks@microsoft.com>
2016-10-13 20:14:38 +0100

    Remove eventhub secrets from code

--- src/Nether.Web/Features/Analytics/EndpointController.cs
+++ src/Nether.Web/Features/Analytics/EndpointController.cs
@@ -3,6 +3,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Nether.Web.Utilities;
+using Microsoft.Extensions.Configuration;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,28 +12,27 @@ namespace Nether.Web.Features.Analytics
     [Route("api/endpoint")]
     public class EndpointController : Controller
     {
-        // GET: api/endpoint
+        private readonly EndpointInfo _endpointInfo;
+
+        public EndpointController(EndpointInfo endpointInfo)
+        {
+            _endpointInfo = endpointInfo;
+        }
         [HttpGet]
         public ActionResult Get()
         {
-            //TODO: Pick up the hardcoded information below from configuration as soon as we have that implemented
-            const string keyName = "game";
-            const string sharedAccessKey = "rxvfLSc98g95pL6+iSAgDya88M+emLKgNXQ0+asRf/8=";
-            const string resource = "https://nether.servicebus.windows.net/analytics/messages";
-            var timeSpan = TimeSpan.FromHours(24);
-
-            var validUntilUtc = DateTime.UtcNow + timeSpan;
+            var validUntilUtc = DateTime.UtcNow + _endpointInfo.Ttl;
 
             var authorization = SharedAccessSignatureTokenProviderEx.GetSharedAccessSignature(
-                keyName,
-                sharedAccessKey,
-                resource,
-                timeSpan);
+                _endpointInfo.KeyName,
+                _endpointInfo.AccessKey,
+                _endpointInfo.Resource,
+                _endpointInfo.Ttl);
 
             var result = new AnalyticsEndpointInfoResponseModel()
             {
                 HttpVerb = "POST",
-                Url = resource,
+                Url = _endpointInfo.Resource,
                 ContentType = "application/json",
                 Authorization = authorization,
                 ValidUntilUtc = validUntilUtc
---
+++ src/Nether.Web/Features/Analytics/EndpointInfo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nether.Web.Features.Analytics
+{
+    public class EndpointInfo
+    {
+        public string KeyName { get; set; }
+        public string AccessKey { get; set; }
+        public string Resource { get; set; }
+        public TimeSpan Ttl { get; set; }
+    }
+
+    public static class EndpointInfoServicesExtensions
+    {
+        public static IServiceCollection AddEndpointInfo(this IServiceCollection services, IConfiguration configuration, string key)
+        {
+            var endpointInfo = GetEndpointInfo(configuration.GetSection(key));
+            services.AddSingleton(endpointInfo);
+            return services;
+        }
+        public static EndpointInfo GetEndpointInfo(IConfiguration configuration)
+        {
+            return new EndpointInfo
+            {
+                KeyName = configuration["KeyName"],
+                AccessKey = configuration["AccessKey"],
+                Resource = configuration["Resource"],
+                Ttl = TimeSpan.Parse(configuration["Ttl"])
+            };
+        }
+    }
+}
