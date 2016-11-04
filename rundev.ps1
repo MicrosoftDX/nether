388c3d3ea278ed0d3ecbfd1900c408756ffe3da3
Stuart Leeks <stuart.leeks@microsoft.com>
2016-11-04 13:06:47 +0000

    Add license to new scripts

--- rundev.ps1
+++ rundev.ps1
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
 $env:ASPNETCORE_ENVIRONMENT="Development" # Set ASP.NET Core environment to Development to enable dev logging, and other dev-only services
 Push-Location
 cd src/Nether.Web
--- rundev.sh
+++ rundev.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+
 export ASPNETCORE_ENVIRONMENT=Development # Set ASP.NET Core environment to Development to enable dev logging, and other dev-only services
 pushd .
 cd src/Nether.Web
