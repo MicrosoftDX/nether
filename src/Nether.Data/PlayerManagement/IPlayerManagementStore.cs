78b2d456aacf60ceae505a572040d6f27787c39a
Anko Duizer <ankodu@microsoft.com>
2016-11-04 17:40:24 +0100

    Added player management v1 interface

---
+++ src/Nether.Data/PlayerManagement/IPlayerManagementStore.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nether.Data.PlayerManagement
+{
+    public interface IPlayerManagementStore
+    {
+        Task SavePlayerAsync(Player player);
+        Task<Player> GetPlayerDetailsAsync(string gamertag);
+
+        Task SaveGroupAsync(Group group);
+        Task<Group> GetGroupDetailsAsync(string groupname);
+        Task AddPlayerToGroupAsync(Group Group, Player player);
+        Task RemovePlayerFromGroupAsync(Group group, Player player);
+    }
+}
---
+++ src/Nether.Data/PlayerManagement/Player.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Nether.Data.PlayerManagement
+{
+    public class Player
+    {
+        public string Gamertag { get; set; }
+        public string Country { get; set; }
+        public string CustomTag { get; set; }
+        public string PlayerImage { get; set; } //to do - change to .NET core Image type
+     }
+}
