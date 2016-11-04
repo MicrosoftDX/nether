ba47ce6517d1fa4c3f0f9b765e4a8001fbc7965e
Anko Duizer <ankodu@microsoft.com>
2016-11-04 17:57:54 +0100

    Change image type to byte[]

--- src/Nether.Data/PlayerManagement/Group.cs
+++ src/Nether.Data/PlayerManagement/Group.cs
@@ -13,6 +13,7 @@ namespace Nether.Data.PlayerManagement
         public string Name { get; set; }
         public string CustomType { get; set; }
         public string Description { get; set; }
+        public byte[] Image { get; set; }
         public List<Player> Players { get; set; }
     }
 }
