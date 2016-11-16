d7e3e74b574fb5f053d89da6c73de448cd8adb16
Kristofer Liljeblad <kristofer.liljeblad@live.com>
2016-11-16 12:56:12 +0100

    Added project to version control SQL Database Schema

---
+++ src/Nether.Data.Sql.Schema/Scores.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Scores]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [Score] INT NOT NULL, 
+    [GamerTag] NVARCHAR(50) NOT NULL, 
+    [CutomTag] NVARCHAR(50) NULL, 
+    [DateAchieved] DATETIME NOT NULL DEFAULT GETUTCDATE() 
+)
+
+GO
+
+CREATE INDEX [IX_Scores_1] ON [dbo].[Scores] ([DateAchieved], [GamerTag], [Score] DESC)
