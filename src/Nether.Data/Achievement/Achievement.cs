10f2da01d190775a4c14bc816bc8d5c4d560dbce
Anko Duizer <ankodu@microsoft.com>
2016-11-18 16:40:31 +0100

    Achievement basics

---
+++ src/Nether.Data/Achievement/Achievement.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nether.Data.Achievement
+{
+
+    public enum State { Hidden, Revealed, Unlocked }
+
+    public class Achievement
+    {
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+        public int Order { get; set; }
+        public bool Incremental { get; set; }
+        public State DefaultState { get; set; }
+
+    }
+}
+
