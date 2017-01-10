7c6835960f00b2cc93abb890e51dab8680ffc877
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-10 13:20:29 +0000

    Avoid duplicate Console logger registration

--- src/Nether.Web/Startup.cs
+++ src/Nether.Web/Startup.cs
@@ -103,7 +103,6 @@ namespace Nether.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             // TODO - this code was copied from Identity Server sample. Need to understand why the map is cleared!
