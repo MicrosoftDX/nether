e98c1c7dd774d493a3dd8cadc2780d6e8c0ef6dc
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-11 21:27:15 +0000

    Add GET api/identity/users/{id}

---
+++ src/Nether.Web/Features/Identity/Models/UserResponseModel.cs
@@ -0,0 +1,47 @@
+﻿using Nether.Data.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nether.Web.Features.Identity.Models
+{
+    public class UserResponseModel
+    {
+        public UserModel User { get; set; }
+
+        public static UserResponseModel MapFrom(User user)
+        {
+            return new UserResponseModel
+            {
+                User = new UserModel
+                {
+                    UserId = user.UserId,
+                    Role = user.Role,
+                    Active = user.IsActive,
+                    Logins = user.Logins.Select(l => new UserLoginModel
+                    {
+                        ProviderType = l.ProviderType,
+                        ProviderId = l.ProviderId,
+                        ProviderData = l.ProviderData
+                    })
+                    .ToList()
+                }
+            };
+        }
+        public class UserModel
+        {
+            public bool Active { get; set; }
+            public string Role { get; set; }
+            public string UserId { get; set; }
+            public List<UserLoginModel> Logins { get; set; }
+        }
+        public class UserLoginModel
+        {
+            public string ProviderData { get; set; }
+            public string ProviderId { get; set; }
+            public string ProviderType { get; set; }
+        }
+    }
+
+}
