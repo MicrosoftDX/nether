6674e8045a4ecadd4cc853bc75d10366a7383c09
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-13 19:28:57 +0000

    Add AsyncLock/AsyncSemaphore

---
+++ src/Nether.Common/Async/AsyncLock.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nether.Common.Async
+{
+    // based on https://blogs.msdn.microsoft.com/pfxteam/2012/02/12/building-async-coordination-primitives-part-6-asynclock/
+    public class AsyncLock
+    {
+        private readonly AsyncSemaphore _semaphore;
+        private readonly Task<Releaser> _releaser;
+
+        public AsyncLock()
+        {
+            _semaphore = new AsyncSemaphore(1);
+            _releaser = Task.FromResult(new Releaser(this));
+        }
+        public Task<Releaser> LockAsync()
+        {
+            var wait = _semaphore.WaitAsync();
+            return wait.IsCompleted ?
+                _releaser :
+                wait.ContinueWith((_, state) => new Releaser((AsyncLock)state),
+                    this, CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+        }
+        public struct Releaser : IDisposable
+        {
+            private readonly AsyncLock m_toRelease;
+
+            internal Releaser(AsyncLock toRelease) { m_toRelease = toRelease; }
+
+            public void Dispose()
+            {
+                if (m_toRelease != null)
+                    m_toRelease._semaphore.Release();
+            }
+        }
+    }
+}
---
+++ src/Nether.Common/Async/AsyncSemaphore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nether.Common.Async
+{
+    // based on https://blogs.msdn.microsoft.com/pfxteam/2012/02/12/building-async-coordination-primitives-part-5-asyncsemaphore/
+    public class AsyncSemaphore
+    {
+        private readonly Queue<TaskCompletionSource<bool>> _waiters = new Queue<TaskCompletionSource<bool>>();
+        private int _currentCount;
+
+        public AsyncSemaphore(int initialCount)
+        {
+            if (initialCount < 0)
+                throw new ArgumentOutOfRangeException("initialCount");
+            _currentCount = initialCount;
+        }
+        public Task WaitAsync()
+        {
+            lock (_waiters)
+            {
+                if (_currentCount > 0)
+                {
+                    --_currentCount;
+                    return Task.CompletedTask;
+                }
+                else
+                {
+                    var waiter = new TaskCompletionSource<bool>();
+                    _waiters.Enqueue(waiter);
+                    return waiter.Task;
+                }
+            }
+        }
+        public void Release()
+        {
+            TaskCompletionSource<bool> toRelease = null;
+            lock (_waiters)
+            {
+                if (_waiters.Count > 0)
+                    toRelease = _waiters.Dequeue();
+                else
+                    ++_currentCount;
+            }
+            if (toRelease != null)
+                toRelease.SetResult(true);
+        }
+    }
+}
