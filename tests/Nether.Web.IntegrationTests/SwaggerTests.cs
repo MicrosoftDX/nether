395d5776317ec24344e87adec3f83b8527812963
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-18 16:51:59 +0000

    Add integration test to ensure SwaggerGen runs ok

---
+++ tests/Nether.Web.IntegrationTests/SwaggerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Nether.Web.IntegrationTests
+{
+    public class SwaggerTests
+    {
+        [Fact]
+        public async Task GET_Swagger_returns_200_OK()
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("http://localhost:5000/api/swagger/v0.1/swagger.json");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
