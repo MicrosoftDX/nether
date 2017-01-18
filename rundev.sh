e98afcbdaa0d11acd5cffb89d8f2e95736a1c823
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-18 10:11:17 +0000

    Remove integration test client configs
    Remove the client configurations for integration tests from the appsettings.json. They are now added via the project settings when running from VS, or the rundev.ps1/sh when running from terminal

--- rundev.sh
+++ rundev.sh
@@ -26,7 +26,24 @@ else
     npm install -g gulp
 fi
 
+# Set environment variables for development and testing__
 export ASPNETCORE_ENVIRONMENT=Development # Set ASP.NET Core environment to Development to enable dev logging, and other dev-only services
+
+# The following settings configure the Identity clients for integration tests
+# They are also in the launchSettings.json for Nether.Web for the VS experience
+export Identity__Clients__clientcreds-test__Name="Test Client for client credentials flow"
+export Identity__Clients__clientcreds-test__AllowedGrantTypes="client_credentials"
+export Identity__Clients__clientcreds-test__ClientSecrets="devsecret"
+export Identity__Clients__clientcreds-test__AllowedScopes="openid, profile, nether-all"
+
+export Identity__Clients__resourceowner-test__AllowedGrantTypes="password"
+export Identity__Clients__resourceowner-test__ClientSecrets="devsecret"
+export Identity__Clients__resourceowner-test__AllowedScopes="nether-all"
+
+export Identity__Clients__customgrant-test__AllowedGrantTypes="fb-usertoken"
+export Identity__Clients__customgrant-test__ClientSecrets="devsecret"
+export Identity__Clients__customgrant-test__AllowedScopes="nether-all"
+
 pushd .
 cd src/Nether.Web
 npm install
