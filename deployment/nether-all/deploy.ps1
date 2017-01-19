5f5a4b21bcd0986013f5b6f41fda08eec8067b32
Ivan Gavryliuk <ivan@elastacloud.com>
2017-01-19 10:12:23 +0000

    fix dependencies

--- deployment/nether-all/deploy.ps1
+++ deployment/nether-all/deploy.ps1
@@ -7,12 +7,19 @@ param
 #Login-AzureRmAccount
 
 <#
-    - from Nether.Web run "dotnet publish -c Release"    
+    - from Nether.Web run "dotnet publish -c Release"
 #>
 
 #uncomment sample to delete previous resource groups by prefix
-Get-AzureRmResourceGroup | Where-Object { $_.ResourceGroupName.StartsWith("nether-deploy-") } | Remove-AzureRmResourceGroup -Force
+#Get-AzureRmResourceGroup | Where-Object { $_.ResourceGroupName.StartsWith("nether-deploy-") } | Remove-AzureRmResourceGroup -Force
 
 #deploy from template
 $rg = New-AzureRmResourceGroup -Name $ResourceGroupName -Location "ukwest"
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile "$PSScriptRoot\template.json"
+
+<#
+    Template notes:
+    
+    - MSDeploy extension conflicts with configuration deployment for website, therefore you must have proper dependencies set up
+      i.e. MSDeploy depends on website, configuraton depends on MSDeploy success.
+#>
