34e6dda077e5c4567bbf5d10de15875ab23f3f64
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-19 13:14:25 +0000

    Add logic to delete old resource group (testing)

--- deployment/nether-all/deploy.ps1
+++ deployment/nether-all/deploy.ps1
@@ -6,5 +6,13 @@ param
 
 #Login-AzureRmAccount
 
+<#
+    - from Nether.Web run "dotnet publish -c Release"    
+#>
+
+#uncomment sample to delete previous resource groups by prefix
+Get-AzureRmResourceGroup | Where-Object { $_.ResourceGroupName.StartsWith("nether-deploy-") } | Remove-AzureRmResourceGroup -Force
+
+#deploy from template
 $rg = New-AzureRmResourceGroup -Name $ResourceGroupName -Location "ukwest"
-New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile "template.json"
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateFile "$PSScriptRoot\template.json"
