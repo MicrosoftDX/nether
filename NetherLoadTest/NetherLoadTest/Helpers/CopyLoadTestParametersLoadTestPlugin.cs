564db1c79444b98eae1ee2b825801c6a2992cc3b
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-25 18:56:26 +0000

    Add CopyLoadTestParameters plugin
    Plugin copies load test context properties to the test context properties for individual tests. This allows things like setting the base url in a single place in the load test run settings.

---
+++ NetherLoadTest/NetherLoadTest/Helpers/CopyLoadTestParametersLoadTestPlugin.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.LoadTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetherLoadTest.Helpers
+{
+    [DisplayName("Copy load test parameters")]
+    [Description("Copies the load test context parameters to the test context for individual tests")]
+    public class CopyLoadTestParametersLoadTestPlugin : ILoadTestPlugin
+    {
+        private LoadTest _loadTest;
+
+        public void Initialize(LoadTest loadTest)
+        {
+            _loadTest = loadTest;
+
+            loadTest.TestStarting += LoadTest_TestStarting;
+        }
+
+        private void LoadTest_TestStarting(object sender, TestStartingEventArgs e)
+        {
+            foreach (var property in _loadTest.Context)
+            {
+                e.TestContextProperties.Add(property);
+            }
+        }
+    }
+}
