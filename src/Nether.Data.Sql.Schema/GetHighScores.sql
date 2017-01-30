ed2ea3704efbd6ce4f81bcd7712a618ed1dd2097
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-30 07:22:13 -0800

    Add leaderboard stored procs

---
+++ src/Nether.Data.Sql.Schema/GetHighScores.sql
@@ -0,0 +1,27 @@
+﻿CREATE PROCEDURE [dbo].[GetHighScores]
+	@StartRank int = 0,
+	@Count int
+AS
+SELECT 
+	Score, 
+	Gamertag, 
+	CustomTag, 
+	Ranking 
+FROM
+	(SELECT 
+		Score, 
+		Gamertag, 
+		CustomTag, 
+		ROW_NUMBER() OVER(ORDER BY Score DESC) AS Ranking 
+		FROM (
+			SELECT 
+				Gamertag, 
+				MAX(Score) AS Score, 
+				MAX(CustomTag) AS CustomTag 
+			FROM Scores 
+			GROUP BY GamerTag
+		) AS T 
+	) AS T2
+WHERE Ranking BETWEEN @StartRank AND (@StartRank + @Count)
+ORDER BY Score DESC
+RETURN 0
---
+++ src/Nether.Data.Sql.Schema/GetScoresAroundPlayer.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].GetScoresAroundPlayer
+	@Gamertag NVARCHAR(50),
+	@Radius INT
+AS
+DECLARE @PlayerRank int
+
+EXEC GetPlayerRank @Gamertag, @PlayerRank OUTPUT
+
+IF (@PlayerRank >= 0)
+BEGIN
+	SELECT 
+		* 
+	FROM (
+		SELECT 
+			Gamertag, 
+			MAX(Score) AS Score, 
+			MAX(CustomTag) AS CustomTag, 
+			ROW_NUMBER() OVER (ORDER BY MAX(Score) DESC) AS Ranking
+		FROM Scores 
+		GROUP BY GamerTag
+	) AS T 
+	WHERE Ranking BETWEEN (@PlayerRank-@Radius) AND (@PlayerRank + @Radius)
+END
+RETURN 0
