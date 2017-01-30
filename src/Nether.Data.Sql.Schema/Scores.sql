2d322511aaa4f61645bc992de3135549843ed396
Stuart Leeks <stuart.leeks@microsoft.com>
2017-01-30 07:21:57 -0800

    Rename Scores.GamerTag to Gamertag

--- src/Nether.Data.Sql.Schema/Scores.sql
+++ src/Nether.Data.Sql.Schema/Scores.sql
@@ -5,11 +5,11 @@ CREATE TABLE [dbo].[Scores]
 (
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
     [Score] INT NOT NULL, 
-    [GamerTag] NVARCHAR(50) NOT NULL, 
+    [Gamertag] NVARCHAR(50) NOT NULL, 
     [CustomTag] NVARCHAR(50) NULL, 
     [DateAchieved] DATETIME NOT NULL DEFAULT GETUTCDATE() 
 )
 
 GO
 
-CREATE INDEX [IX_Scores_1] ON [dbo].[Scores] ([DateAchieved], [GamerTag], [Score] DESC)
+CREATE INDEX [IX_Scores_1] ON [dbo].[Scores] ([DateAchieved], [Gamertag], [Score] DESC)
