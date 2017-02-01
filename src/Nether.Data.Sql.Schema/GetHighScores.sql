953eb195129a7c900b82e023962de16b82c767d1
Stuart Leeks <stuart.leeks@microsoft.com>
2017-02-01 13:38:01 -0800

    Add license header

--- src/Nether.Data.Sql.Schema/GetHighScores.sql
+++ src/Nether.Data.Sql.Schema/GetHighScores.sql
@@ -1,4 +1,7 @@
-﻿CREATE PROCEDURE [dbo].[GetHighScores]
+﻿/* Copyright (c) Microsoft. All rights reserved.
+Licensed under the MIT license. See LICENSE file in the project root for full license information. */
+
+CREATE PROCEDURE [dbo].[GetHighScores]
 	@StartRank int = 0,
 	@Count int
 AS
