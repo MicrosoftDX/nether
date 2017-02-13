939a8667802211bc17903d0cfe0c49c1c0164960
Stuart Leeks <stuart.leeks@microsoft.com>
2017-02-13 16:28:15 +0000

    Remove sample license as per discussion with Anko

--- src/Nether.Web/Features/IdentityUi/Account/LoggedOutViewModel.cs
+++ src/Nether.Web/Features/IdentityUi/Account/LoggedOutViewModel.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
 namespace Nether.Web.Features.IdentityUi
 {
     public class LoggedOutViewModel
