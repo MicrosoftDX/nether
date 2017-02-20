32befdaf977cc5836a0236bb4fd9c26ae2023211
Stuart Leeks <stuart.leeks@microsoft.com>
2017-02-20 11:14:30 +0000

    Remove angular-in-memory-web-api

--- src/Nether.Web/wwwroot/Features/AdminWebUI/systemjs.config.js
+++ src/Nether.Web/wwwroot/Features/AdminWebUI/systemjs.config.js
@@ -24,8 +24,8 @@
             '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
 
             // other libraries
-            'rxjs': 'npm:rxjs',
-            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
+            'rxjs': 'npm:rxjs'
+            // 'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
