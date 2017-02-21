118cd0a78ce576698fc35a675407d6aee6c398e3
Vito Flavio Lorusso <vf.lorusso@live.com>
2017-02-21 10:14:38 +0100

    Dev setup edit to adjust first time build instructions

--- src/Nether.Web/gulpfile.js
+++ src/Nether.Web/gulpfile.js
@@ -14,9 +14,9 @@ gulp.task('default', function () {
 // compiles ts files in js and outputs them in the same folder
 gulp.task("compiletsforadminui", function () {
     console.log('compile ts ui files into js');
-    var tsResult = gulp.src(["./wwwroot/Features/AdminWebUi/app/**/*.ts"], {base: "."}) 
-        .pipe(tsProject()); 
-    return tsResult.js.pipe(gulp.dest('.'));
+    var tsResult = gulp.src(["./wwwroot/Features/AdminWebUi/app/**/*.ts"], {base: "."}) 
+        .pipe(tsProject()); 
+    return tsResult.js.pipe(gulp.dest('.'));
 });
 
 gulp.task("npmtolib", () => {
