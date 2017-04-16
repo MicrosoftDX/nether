94ec649cd108552f3f226ca22c8005c612e836db
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-04-16 23:30:38 +0300

    First draft of Unity SDK for Nether

---
+++ src/SDKs/Unity/Assets/NetherSDK/Models/Endpoint.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetherSDK.Models
+{
+    [Serializable]
+    public class Endpoint
+    {
+        public string httpVerb;
+        public string url;
+        public string contentType;
+        public string authorization;
+        public string validUntilUtc;
+    }
+}
