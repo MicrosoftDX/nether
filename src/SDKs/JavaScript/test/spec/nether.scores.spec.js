42b316ce572f8c715b76ecdee47de4264712c890
Andy Cross <andy@elastacloud.com>
2017-04-26 18:57:33 +0100

    test dropMyScore

--- src/SDKs/JavaScript/test/spec/nether.scores.spec.js
+++ src/SDKs/JavaScript/test/spec/nether.scores.spec.js
@@ -77,3 +77,66 @@ describe("nether scores addScore", function() {
 
   });
 });
+
+describe("nether scores dropMyScore", function() {
+  beforeEach(function() {
+    nether.common.ajax = jasmine.createSpy().and.callFake(function(args) {
+        args.callback(204, "");
+      });
+  });
+
+  it("exposes dropMyScore", function() {
+    expect(nether.scores.dropMyScore).toBeDefined();
+  });
+
+  it("sends an ajax message", function() {    
+    nether.scores.dropMyScore(jasmine.createSpy("successCallback"));
+    
+    expect(nether.common.ajax).toHaveBeenCalled();
+  });
+
+  it("sends an ajax message to a sensible place", function() {    
+    //reinit to blank baseurl
+    nether.init({ 
+      "netherClientId" : "Kristofer",
+      "netherClientSecret" : "Anko",
+      "facebookAppId" : "Stuart",
+      "netherBaseUrl" : ""
+    }, function(){}, function(){}, document);
+    nether.scores.dropMyScore(jasmine.createSpy("successCallback"));
+    
+    expect(nether.common.ajax).toHaveBeenCalledWith(jasmine.objectContaining({"url":"/api/scores"}));
+  });
+
+  it("sends an ajax message to a configurable place", function() {    
+    nether.init(exampleConfig, function(){}, function(){}, document);
+    nether.scores.dropMyScore(jasmine.createSpy("successCallback"));    
+    expect(nether.common.ajax).toHaveBeenCalledWith(jasmine.objectContaining({"url": exampleConfig.netherBaseUrl + "/api/scores"}));
+  });
+
+  it("calls the supplied callback", function() {    
+    var callback = jasmine.createSpy("successCallback");
+    nether.scores.dropMyScore(callback);
+    
+    expect(callback).toHaveBeenCalled();
+  });
+
+
+  it("calls the supplied callback with true in success case", function() {    
+    var callback = jasmine.createSpy("successCallback");
+    nether.scores.dropMyScore(callback);
+    
+    expect(callback).toHaveBeenCalledWith(true);
+  });
+
+  it("handles failed http responses gracefully", function() {
+    nether.common.ajax = jasmine.createSpy().and.callFake(function(args) {
+        args.callback(500, {});
+      });
+
+    var genericCallback = jasmine.createSpy("genericCallback");
+    nether.scores.dropMyScore(genericCallback);
+    expect(genericCallback).toHaveBeenCalledWith(false);
+
+  });
+});
