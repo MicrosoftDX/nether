32fa8617f48ee85da5f9efdf3efbeef3405669de
Andy Cross <andy@elastacloud.com>
2017-04-26 21:30:41 +0100

    complete tests for player

--- src/SDKs/JavaScript/test/spec/nether.player.spec.js
+++ src/SDKs/JavaScript/test/spec/nether.player.spec.js
@@ -218,3 +218,136 @@ describe("nether player deletePlayer", function() {
 
   });
 });
+
+describe("nether player getPlayerState", function() {
+  beforeEach(function() {
+    nether.common.ajax = jasmine.createSpy().and.callFake(function(args) {
+        args.callback(200, "{ \"gamertag\":\"BeautifulStranger27\", \"state\":{ \"state\":123 } }");
+      });
+  });
+
+  it("exposes getState", function() {
+    expect(nether.player.getState).toBeDefined();
+  });
+
+  it("sends an ajax message", function() {    
+    nether.player.getState(jasmine.createSpy("successCallback"));
+    
+    expect(nether.common.ajax).toHaveBeenCalled();
+  });
+
+  it("sends an ajax message to a sensible place", function() {    
+    //reinit to blank baseurl
+    nether.init({ 
+      "netherClientId" : "Kristofer",
+      "netherClientSecret" : "Anko",
+      "facebookAppId" : "Stuart",
+      "netherBaseUrl" : ""
+    }, function(){}, function(){}, document);
+    nether.player.getState(jasmine.createSpy("successCallback"));
+    
+    expect(nether.common.ajax).toHaveBeenCalledWith(jasmine.objectContaining({"url":"/api/player/state"}));
+  });
+
+  it("sends an ajax message to a configurable place", function() {    
+    nether.init(exampleConfig, function(){}, function(){}, document);
+    nether.player.getState(jasmine.createSpy("successCallback"));    
+    expect(nether.common.ajax).toHaveBeenCalledWith(jasmine.objectContaining({"url": exampleConfig.netherBaseUrl + "/api/player/state"}));
+  });
+
+  it("assigns the gamerTag", function() {    
+    nether.init(exampleConfig, function(){}, function(){}, document);
+    nether.player.deletePlayer(jasmine.createSpy("successCallback"));    
+    expect(nether.player.gamertag).toBe("BeautifulStranger27");
+  });
+
+  it("assigns the state", function() {    
+    nether.init(exampleConfig, function(){}, function(){}, document);
+    nether.player.deletePlayer(jasmine.createSpy("successCallback"));    
+    expect(nether.player.state).toBe(123);
+  });
+
+  it("calls the supplied callback", function() {    
+    var callback = jasmine.createSpy("successCallback");
+    nether.player.deletePlayer(callback);
+    
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("handles failed http responses with returned Error class", function() {
+    nether.common.ajax = jasmine.createSpy().and.callFake(function(args) {
+        args.callback(500, {});
+      });
+
+    var genericCallback = jasmine.createSpy("genericCallback");
+    nether.player.deletePlayer(genericCallback);
+    expect(genericCallback).toHaveBeenCalledWith(false); //todo: assert that nullity is correct here
+
+  });
+});
+
+describe("nether player setPlayerState", function() {
+  beforeEach(function() {
+    nether.common.ajax = jasmine.createSpy().and.callFake(function(args) {
+        args.callback(204, "");
+      });
+  });
+
+  it("exposes setState", function() {
+    expect(nether.player.setState).toBeDefined();
+  });
+
+  it("sends an ajax message", function() {    
+    nether.player.setState({}, jasmine.createSpy("successCallback"));
+    
+    expect(nether.common.ajax).toHaveBeenCalled();
+  });
+
+  it("sends an ajax message to a sensible place", function() {    
+    //reinit to blank baseurl
+    nether.init({ 
+      "netherClientId" : "Kristofer",
+      "netherClientSecret" : "Anko",
+      "facebookAppId" : "Stuart",
+      "netherBaseUrl" : ""
+    }, function(){}, function(){}, document);
+    nether.player.setState({}, jasmine.createSpy("successCallback"));
+    
+    expect(nether.common.ajax).toHaveBeenCalledWith(jasmine.objectContaining({"url":"/api/player/state"}));
+  });
+
+  it("sends an ajax message to a configurable place", function() {    
+    nether.init(exampleConfig, function(){}, function(){}, document);
+    nether.player.setState({}, jasmine.createSpy("successCallback"));    
+    expect(nether.common.ajax).toHaveBeenCalledWith(jasmine.objectContaining({"url": exampleConfig.netherBaseUrl + "/api/player/state"}));
+  });
+
+  it("sends an ajax message containing the state", function() {    
+    nether.init(exampleConfig, function(){}, function(){}, document);
+    nether.player.setState({ "someState":"yesItIs"}, jasmine.createSpy("successCallback"));    
+    expect(nether.common.ajax).toHaveBeenCalledWith(
+      jasmine.objectContaining({"data": 
+        jasmine.objectContaining({ "state" : 
+          jasmine.objectContaining({ "someState" : "yesItIs"})
+           })
+    }));
+  });
+
+  it("calls the supplied callback", function() {    
+    var callback = jasmine.createSpy("successCallback");
+    nether.player.setState({}, callback);
+    
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("handles failed http responses with returned Error class", function() {
+    nether.common.ajax = jasmine.createSpy().and.callFake(function(args) {
+        args.callback(500, {});
+      });
+
+    var genericCallback = jasmine.createSpy("genericCallback");
+    nether.player.setState({}, genericCallback);
+    expect(genericCallback).toHaveBeenCalledWith(jasmine.any(Error)); //todo: assert that nullity is correct here
+
+  });
+});
