637936448001df4f12b765b41ca47ac66ac0523b
Andy Cross <andy@elastacloud.com>
2017-04-26 18:54:35 +0100

    smaller spec files

---
+++ src/SDKs/JavaScript/test/spec/common.spec.js
@@ -0,0 +1,6 @@
+const exampleConfig = { 
+      "netherClientId" : "Kristofer",
+      "netherClientSecret" : "Anko",
+      "facebookAppId" : "Stuart",
+      "netherBaseUrl" : "https://andy.and.james/from/elastacloud"
+    };
