3b4f1e7e589392903f16ff5f21edb0192cfe979d
Nava Vaisman Levy <navale@microsoft.com>
2017-05-07 13:29:16 +0300

    formatter

--- src/Nether.Web/Features/Leaderboard/Models/Score/ScorePostRequestModel.cs
+++ src/Nether.Web/Features/Leaderboard/Models/Score/ScorePostRequestModel.cs
@@ -12,7 +12,7 @@ namespace Nether.Web.Features.Leaderboard.Models.Score
         /// </summary>
         [Required]
         public string Country { get; set; }
-       
+
         /// <summary>
         /// Achieved score
         /// </summary>
