fe9a6c236cce396bf9f4f5157ef22c7dff2e8996
Kristofer Liljeblad <kristol@microsoft.com>
2017-05-08 23:26:31 +0200

    implemented BingLocationLookupHandler

--- src/Nether.Analytics.Bing/BingLocationLookupHandler.cs
+++ src/Nether.Analytics.Bing/BingLocationLookupHandler.cs
@@ -3,14 +3,72 @@
 
 using System;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
 
 namespace Nether.Analytics.Bing
 {
     public class BingLocationLookupHandler : IMessageHandler
     {
-        public Task<MessageHandlerResluts> ProcessMessageAsync(string pipelineName, int idx, Message msg)
+        string _bingMapsKey;
+        string _latProperty;
+        string _lonProperty;
+
+        public BingLocationLookupHandler(string bingMapsKey, string latProperty, string lonProperty)
         {
-            throw new NotImplementedException();
+            _bingMapsKey = bingMapsKey;
+            _latProperty = latProperty;
+            _lonProperty = lonProperty;
+        }
+
+        public async Task<MessageHandlerResluts> ProcessMessageAsync(Message msg, string pipelineName, int idx)
+        {
+            //TODO: Implement cache layer that caches result based on a surounding for example use a geohash if present
+
+            var lat = msg.Properties[_latProperty];
+            var lon = msg.Properties[_lonProperty];
+
+            var client = new WebClient();
+            var bingUrl = $"http://dev.virtualearth.net/REST/v1/Locations/{lat},{lon}?key={_bingMapsKey}";
+            string bingResult;
+
+            try
+            {
+                bingResult = await client.DownloadStringTaskAsync(bingUrl);
+            }
+            catch (Exception ex)
+            {
+                //TODO: Replace all console logging of exceptions to generic log solution
+                Console.WriteLine("An exception occurred while calling Bing to Lookup coordinates");
+                Console.WriteLine(ex);
+
+                return MessageHandlerResluts.FailStopProcessing;
+            }
+
+            try
+            {
+                var json = JObject.Parse(bingResult);
+
+                var address = json["resourceSets"][0]["resources"][0]["address"];
+                var country = (string)address["countryRegion"];
+                var district = (string)address["adminDistrict"];
+                var city = (string)address["locality"];
+
+                msg.Properties.Add("country", country);
+                msg.Properties.Add("district", district);
+                msg.Properties.Add("city", city);
+
+                return MessageHandlerResluts.Success;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception occurred while trying to parse Location Lookup results from Bing");
+                Console.WriteLine(ex);
+
+                return MessageHandlerResluts.FailStopProcessing;
+            }
         }
     }
 }
