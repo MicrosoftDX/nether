00488dd992d94896b0e8b95cc512cc0410a7e13a
Kristofer Liljeblad <kristol@microsoft.com>
2017-05-09 04:40:42 +0200

    Removed any references to Nether.Analytics from Nether.sln and projects within that solution

--- src/Nether.Integration/Analytics/IAnalyticsIntegrationClient.cs
+++ src/Nether.Integration/Analytics/IAnalyticsIntegrationClient.cs
@@ -1,18 +1,19 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
+
 using System.Threading.Tasks;
-using Nether.Analytics.GameEvents;
 
 namespace Nether.Integration.Analytics
 {
     public interface IAnalyticsIntegrationClient
     {
-        Task SendGameEventAsync(IGameEvent gameEvent);
+        Task SendGameEventAsync(INetherMessage gameEvent);
+    }
+
+    public interface INetherMessage
+    {
+
     }
 }
 
