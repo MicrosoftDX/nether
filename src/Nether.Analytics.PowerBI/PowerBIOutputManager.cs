31cdc7571c1cf2fd54825b208364446cb3c65b5d
Kristofer Liljeblad <kristol@microsoft.com>
2017-05-11 14:47:59 +0200

    Default implementations of OutputManagers for added projects

---
+++ src/Nether.Analytics.PowerBI/PowerBIOutputManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Nether.Analytics.PowerBI
+{
+    public class PowerBIOutputManager : IOutputManager
+    {
+        //TODO: Implement a working solution for outputting to PowerBI.
+        // Take a look at the implementation for DataLakeStoreOutputManager for inspiration and sync
+        public Task FlushAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OutputMessageAsync(string pipelineName, int idx, Message msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
