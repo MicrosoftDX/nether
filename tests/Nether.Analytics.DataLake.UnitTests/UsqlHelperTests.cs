72aedcaa2cdaf8e3bdeec219eab5a09c981de9b7
Kristofer Liljeblad <kristol@microsoft.com>
2017-05-11 14:49:19 +0200

    CodeFormatter

--- tests/Nether.Analytics.DataLake.UnitTests/UsqlHelperTests.cs
+++ tests/Nether.Analytics.DataLake.UnitTests/UsqlHelperTests.cs
@@ -1,4 +1,7 @@
-﻿using Nether.Analytics.DataLake.UnitTests.Properties;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Nether.Analytics.DataLake.UnitTests.Properties;
 using System;
 using System.Collections.Generic;
 using Xunit;
