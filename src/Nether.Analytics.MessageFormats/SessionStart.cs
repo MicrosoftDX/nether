1bf42cd001088d1eff192ec276adacee97290ccc
Kristofer Liljeblad <kristol@microsoft.com>
2017-05-17 11:55:01 -0700

    Code Formatter

--- src/Nether.Analytics.MessageFormats/SessionStart.cs
+++ src/Nether.Analytics.MessageFormats/SessionStart.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Nether.Analytics.MessageFormats
 {
-    class SessionStart : INetherMessage
+    internal class SessionStart : INetherMessage
     {
         public string Type => "session-start";
         public string Version => "1.0.0";
