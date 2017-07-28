8bfffc8d67e110fac252eb81d00fb02db450a2d7
Stuart Leeks <stuart.leeks@microsoft.com>
2017-07-28 10:51:01 +0100

    Update UserController to use FacebookGraphService

--- src/Nether.Web/Features/Identity/Controllers/UserController.cs
+++ src/Nether.Web/Features/Identity/Controllers/UserController.cs
@@ -23,15 +23,16 @@ namespace Nether.Web.Features.Identity
     public class UserController : ControllerBase
     {
         private readonly IUserStore _userStore;
-        private ILogger<UserController> _logger;
+        private readonly FacebookGraphService _facebookGraphService;
+        private readonly ILogger _logger;
 
         public UserController(IUserStore userStore,
-            IConfiguration configuration,
+            FacebookGraphService facebookGraphService,
             ILogger<UserController> logger)
         {
             _userStore = userStore;
+            _facebookGraphService = facebookGraphService;
             _logger = logger;
-            _configuration = configuration;
         }
 
         /// <summary>
@@ -113,18 +114,18 @@ namespace Nether.Web.Features.Identity
                 return this.ValidationFailed(new ErrorDetail("facebookToken", "Missing Facebook token."));
             }
 
-            // we need to verify the token first
+           // we need to verify the token first
             // since only Facebook is supported, we default to it, but in the future, this will likely move
             // to a provider-based pattern
-            var providerId = await GetFacebookUserIdFromTokenAsync(userLoginModel.FacebookToken);
-
-            if (string.IsNullOrEmpty(providerId))
+            var result = await _facebookGraphService.TokenDebugAsync(userLoginModel.FacebookToken);
+            if (!result.IsValid || string.IsNullOrEmpty(result.UserId))
             {
+                _logger.LogTrace("Facebook token validation failed: {0}", result.Error.Message);
                 return this.ValidationFailed(new ErrorDetail("facebookToken", "Facebook token is either invalid or could not be validated."));
             }
 
-            user.Logins = user.Logins ?? new List<Login>();
-            var login = user.Logins?.FirstOrDefault(l => l.ProviderType == providerType && l.ProviderId == providerId);
+            var providerId = result.UserId;
+            var login = user.Logins.FirstOrDefault(l => l.ProviderType == providerType && l.ProviderId == providerId);
             if (login == null)
             {
                 login = new Login
@@ -136,66 +137,13 @@ namespace Nether.Web.Features.Identity
             }
             else
             {
-                // TODO: should we return a validation error here?
-                return NoContent();
+                login.ProviderId = providerId;
             }
 
             await _userStore.SaveUserAsync(user);
 
             //return CreatedAtRoute(nameof(DeleteUserLogin), new { providerType, providerId }, null);
             return CreatedAtRoute(nameof(PostUserLogin), login);
-        }
-
-        private IConfiguration _configuration;
-
-        private async Task<string> GetFacebookUserIdFromTokenAsync(string token)
-        {
-            // Call facebook graph api to validate the token
-
-            // As per https://developers.facebook.com/docs/facebook-login/access-tokens/#apptokens, we're using "appid|appsecret" for the app token
-            var appId = _configuration["Identity:SignInMethods:Facebook:AppId"];
-            var appSecret = _configuration["Identity:SignInMethods:Facebook:AppSecret"];
-            var appToken = appId + "|" + appSecret;
-
-
-            var client = CreateHttpClient();
-            var response = await client.GetAsync($"/debug_token?input_token={token}&access_token={appToken}");
-
-            dynamic body = await response.Content.ReadAsAsync<dynamic>();
-
-            if (body.data == null)
-            {
-                // Get here if (for example) the token is for a different application
-                var message = (string)body.error.message;
-                _logger.LogError("FacebookSignIn: error validating token: {0}", message);
-                return null;
-            }
-
-            bool isValid = (bool)body.data.is_valid;
-            var userId = (string)body.data.user_id;
-            if (!isValid)
-            {
-                var message = (string)body.data.error.message;
-                _logger.LogError("FacebookSignIn: invalid token: {0}", message);
-                return null;
-            }
-
-            return userId;
-        }
-
-        private static HttpClient CreateHttpClient()
-        {
-            return new HttpClient()
-            {
-                BaseAddress = new Uri("https://graph.facebook.com/"),
-                DefaultRequestHeaders =
-                {
-                    Accept =
-                    {
-                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json")
-                    }
-                }
-            };
         }
     }
 }
