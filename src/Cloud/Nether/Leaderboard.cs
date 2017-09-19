99dd012c446e63b0993433bcc6befb4c46c5bd44
Kristofer Liljeblad <kristol@microsoft.com>
2017-09-19 11:18:38 -0700

    Initial setup of new project

---
+++ src/Cloud/Nether/Leaderboard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nether
+{
+    public class Leaderboard
+    {
+        public void SetIfBetter(string leaderboard, string whom, string score)
+        {
+            
+        }
+    }
+}
