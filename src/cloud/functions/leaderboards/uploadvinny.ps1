033627fca59cb8ec72bdc4d95ece00a0df730faf
vinnytheviking <vincent.vanderwalt@gmail.com>
2017-10-05 15:40:21 +0100

    Add files via upload

--- src/cloud/functions/leaderboards/uploadvinny.ps1
+++ src/cloud/functions/leaderboards/uploadvinny.ps1
@@ -1,27 +1,3 @@
-﻿# https://github.com/vinnytheviking/nether/tree/serverless/src/cloud/functions/leaderboards/top-n
-
-
- $functionurl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/function.json"
- $projeccturl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/project.json"
- $runurl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/run.csx"
- $sampleurl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/sample.dat"
-
-
-#$storageDir = $pwd
-        
-
-#New-Item -Type File -Force -Path $PWD | out-null
-
-#$wc = New-Object System.Net.WebClient
-
-#$wc.Encoding = [System.Text.Encoding]::UTF8
-
- #($wc.DownloadString("$url")) | Out-File "function.json"
-
- $wc = New-Object System.Net.WebClient; 
- 
- Add-Content -Path "$pwd\site\wwwroot\leaderboard\function.json" -Value $wc.DownloadString($functionurl);
- Add-Content -Path "$pwd\site\wwwroot\leaderboard\project.json" -Value $wc.DownloadString($projeccturl);
- Add-Content -Path "$pwd\site\wwwroot\leaderboard\run.csx" -Value $wc.DownloadString($runurl);
- Add-Content -Path "$pwd\site\wwwroot\leaderboard\sample.dat" -Value $wc.DownloadString($sampleurl)
-
+﻿$baseurl = "https://raw.githubusercontent.com/MicrosoftDX/nether/serverless/src/cloud/functions/leaderboards/top-n";
+$wc = New-Object System.Net.WebClient; 
+$wc.DownloadString("$baseurl/functiondeploy.txt").Split()| ? { $_ } | % { $folder = Split-Path -Path $_; if (!(Test-Path $folder)) { "Creating folder $folder"; mkdir $folder | Out-Null }; "Downloading $_"; Add-Content $_ -Value $wc.DownloadString("$baseurl/$_"); }
