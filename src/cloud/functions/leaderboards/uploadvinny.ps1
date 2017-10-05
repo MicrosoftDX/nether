2d079ffe8d44e864f2e9e33b996e99cfafa6de3b
vinnytheviking <vincent.vanderwalt@gmail.com>
2017-10-05 14:55:41 +0100

    Add files via upload

--- src/cloud/functions/leaderboards/uploadvinny.ps1
+++ src/cloud/functions/leaderboards/uploadvinny.ps1
@@ -1,12 +1,11 @@
 ﻿# https://github.com/vinnytheviking/nether/tree/serverless/src/cloud/functions/leaderboards/top-n
 
 
- $url = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/function.json"
+ $functionurl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/function.json"
+ $projeccturl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/project.json"
+ $runurl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/run.csx"
+ $sampleurl = "https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/sample.dat"
 
- # https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/function.json
- # https://github.com/vinnytheviking/nether/blob/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/project.json
- # https://github.com/vinnytheviking/nether/blob/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/run.csx
- # https://github.com/vinnytheviking/nether/blob/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/sample.dat
 
 #$storageDir = $pwd
         
@@ -19,5 +18,10 @@
 
  #($wc.DownloadString("$url")) | Out-File "function.json"
 
- $wc = New-Object System.Net.WebClient; New-Item -Type Directory -Force -Path "top-n" | out-null;  Add-Content -Path "$pwd\top-n\vinny.json" -Value $wc.DownloadString("https://raw.githubusercontent.com/vinnytheviking/nether/serverless/src/cloud/functions/leaderboards/top-n/leaderboard/function.json")
+ $wc = New-Object System.Net.WebClient; 
+ 
+ Add-Content -Path "$pwd\site\wwwroot\leaderboard\function.json" -Value $wc.DownloadString($functionurl);
+ Add-Content -Path "$pwd\site\wwwroot\leaderboard\project.json" -Value $wc.DownloadString($projeccturl);
+ Add-Content -Path "$pwd\site\wwwroot\leaderboard\run.csx" -Value $wc.DownloadString($runurl);
+ Add-Content -Path "$pwd\site\wwwroot\leaderboard\sample.dat" -Value $wc.DownloadString($sampleurl)
 
