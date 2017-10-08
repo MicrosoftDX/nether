96983b7e7ab0ea7f60cff1b381257878faf39996
Vincent van der Walt <vivand@microsoft.com>
2017-10-08 18:46:21 +0200

    initial fixes for issue 577

--- src/cloud/functions/leaderboards/top-n/deploy.ps1
+++ src/cloud/functions/leaderboards/top-n/deploy.ps1
@@ -1,3 +1,3 @@
-﻿$baseurl = "https://raw.githubusercontent.com/MicrosoftDX/nether/serverless/src/cloud/functions/leaderboards/top-n";
+﻿$baseurl = "https://raw.githubusercontent.com/MicrosoftDX/nether/serverless/src/cloud/functions/leaderboards/top-n/deploy.ps1";
 $wc = New-Object System.Net.WebClient; 
 $wc.DownloadString("$baseurl/functiondeploy.txt").Split()| ? { $_ } | % { $folder = Split-Path -Path $_; if (!(Test-Path $folder)) { "Creating folder $folder"; mkdir $folder | Out-Null }; "Downloading $_"; Add-Content $_ -Value $wc.DownloadString("$baseurl/$_"); }
