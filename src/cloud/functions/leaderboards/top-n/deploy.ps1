8cea8b64c3e07b7e07a32f5a06bbf4f045ab11e2
Vincent van der Walt <vivand@microsoft.com>
2017-10-08 18:51:25 +0200

    change script for testing deploy

--- src/cloud/functions/leaderboards/top-n/deploy.ps1
+++ src/cloud/functions/leaderboards/top-n/deploy.ps1
@@ -1,3 +1,3 @@
-﻿$baseurl = "https://raw.githubusercontent.com/MicrosoftDX/nether/serverless/src/cloud/functions/leaderboards/top-n/deploy.ps1";
+﻿$baseurl = "https://github.com/vinnytheviking/nether/tree/Issue577/src/cloud/functions/leaderboards/top-n";
 $wc = New-Object System.Net.WebClient; 
 $wc.DownloadString("$baseurl/functiondeploy.txt").Split()| ? { $_ } | % { $folder = Split-Path -Path $_; if (!(Test-Path $folder)) { "Creating folder $folder"; mkdir $folder | Out-Null }; "Downloading $_"; Add-Content $_ -Value $wc.DownloadString("$baseurl/$_"); }
